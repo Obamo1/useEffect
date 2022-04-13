ed6b5cba6739654c6f0c15d0de02643c4d5bed1c
obamo1 <joobamo@gmail.com>
2022-04-13 10:56:36 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics';
+import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+
+
+ 
+ReactDOM.render(
+    <UseEffectCleanup />,
+  
+  document.getElementById('root')
+);
