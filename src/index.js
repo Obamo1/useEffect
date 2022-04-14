5a3baea158114c14c251d39e8043f5f3b25253cd
obamo1 <joobamo@gmail.com>
2022-04-14 14:42:07 +0100

    initial1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics';
-import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+// import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 
 
  
 ReactDOM.render(
-    <UseEffectCleanup />,
+    <UseEffectFetchData />,
   
   document.getElementById('root')
 );
